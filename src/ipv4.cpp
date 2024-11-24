69673de347a9b087cdd809bd03ca4761db099504
Simonaytes <yandexman228@gmail.com>
2024-11-24 18:36:40 +0300

    Change IPv4Header

--- src/ipv4.cpp
+++ src/ipv4.cpp
@@ -10,18 +10,33 @@ enum IPProtocolType {
     STP  = 118
 };
 
+// struct IPv4Package {
+//     uint8_t  ip_hl:4, ip_v:4;
+//     uint8_t  ip_tos;
+//     uint16_t ip_len;
+//     uint16_t ip_id;
+//     uint16_t ip_off;
+//     uint8_t  ip_ttl;
+//     uint8_t  ip_p;
+//     uint16_t ip_sum;
+//     uint32_t ip_src;
+//     uint32_t ip_dst;
+//     void* data;
+// };
+
+
 struct IPv4Package {
-    uint8_t  ip_hl:4, ip_v:4;
-    uint8_t  ip_tos;
-    uint16_t ip_len;
-    uint16_t ip_id;
-    uint16_t ip_off;
-    uint8_t  ip_ttl;
-    uint8_t  ip_p;
-    uint16_t ip_sum;
-    uint32_t ip_src;
-    uint32_t ip_dst;
-    void* data;
+    /* 0 */ unsigned char ip_hl:4, ip_v:4;
+    /* 1 */ unsigned char ip_tos;
+    /* 2 */ unsigned short int ip_len;
+    /* 3 */ unsigned short int ip_id;
+    /* 4 */ unsigned short int ip_off;
+    /* 5 */ unsigned char ip_ttl;
+    /* 6 */ unsigned char ip_p;
+    /* 7 */ unsigned short int ip_sum;
+    /* 8 */ unsigned int ip_src;
+    /* 9 */ unsigned int ip_dst;
+    // void* data;
 };
 
 void processIPv4(void* buffer, size_t size) {
